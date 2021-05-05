4ca8a0deee9ba7f9dab4f729bd54b419a4eff943
m2cci-mohamed-ltifi <m2cci-mohamed-ltifi@users.noreply.github.com>
2021-05-05 17:02:53 +0200

    EventListner

--- my_first_react_app/src/index.js
+++ my_first_react_app/src/index.js
@@ -1,58 +1,50 @@
+import React from "react";
+import ReactDom from "react-dom";
 
-import React from 'react';
-import ReactDom from 'react-dom';
+import "./index.css";
 
-import './index.css'
+const books = [
+  {
+    id: 1,
+    img:
+      "https://m.media-amazon.com/images/M/MV5BODAwNDhhNDItMzNiNC00NjFlLTk3NmEtZDVhYzliNTE1NjI3XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_UY268_CR7,0,182,268_AL_.jpg",
+    title: "What the day owes the night",
+    author: "Yasmina Khedher",
+  },
+  {
+    id: 2,
+    img:
+      "https://www.mercator-ocean.fr/wp-content/uploads/2019/11/Mock-Up_BlueBookCopernicus_2-600x400.jpg",
+    title: "The Blue Book",
+    author: "Mercator Ocean International",
+  },
+];
 
-const Greeting = () => {
-  return  (
-    <div>
-      <h4>Hello World</h4>
-      <ul>
-        <li> 
-          <a href="www.google.com"> Hello</a>
-        </li>
-        <li>
-        <input type="text"/>
-        </li>
-      </ul>
-    </div>
-  
-  );
-}
-function Hello (){
-  return React.createElement('h1',{},'hello world React');
-}
-// Hello1 return a html script using the function Person
-function Hello1 (){
+function BookList() {
   return (
-    <Person/>
-  );
-}
-const Person =() => <h2>Mohamed Ayoub</h2>
-
-/*ReactDom.render  replace the element root 
-// from index.html by the function Hello1
-ReactDom.render(<Hello1/>,document.getElementById('root'));*/
-function BookList(){
-  return(
     <section className="booklist">
-      <Book titre="What the day owes the night"/>
-      <Book author="Yasmina khedher"/>
+      {books.map((book) => {
+        //return <Book img={book.img} title={book.title} author={book.author} />;
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
-const source="https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg";
-const Book = (props) => {
+
+function Book({ img, title, author }) {
+  //const {img,title,author}=props.book;
+  const clickHandler = () => {
+    alert(title);
+  };
   return (
-  <article className="book">
-    <img width="200" height="300"  src={source} alt=""/>
-    <h3>{props.titre}</h3>
-    <h2> { "By " +props.author}</h2>
-  </article>
+    <article className="book">
+      <img src={img} alt="" width="200" height="300" />
+      <h2 onClick={() => console.log(title)}> {title} </h2>
+      <h5> {author}</h5>
+      <button type="button" onClick={clickHandler}>
+        Quick Event
+      </button>
+    </article>
   );
 }
-
-
-
-ReactDom.render(<BookList/>,document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById("root"));
